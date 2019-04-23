794163f723aafdb50ca61502ece7c5439c457828
杨云召 <yangyunzhao@bcbook.cn>
2019-04-23 18:00:20 +0800

    Initial commit

---
+++ app/src/main/java/com/example/accessibilitytest/MyAccessibilityService.java
@@ -0,0 +1,93 @@
+package com.example.accessibilitytest;
+
+import android.accessibilityservice.AccessibilityService;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.accessibility.AccessibilityEvent;
+import android.view.accessibility.AccessibilityNodeInfo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author yyz (杨云召)
+ * @date 2019/4/22
+ * time   16:23
+ * description
+ */
+public class MyAccessibilityService extends AccessibilityService {
+    List<String> stringList = new ArrayList<>();
+
+    {
+        stringList.add("继续安装");
+        stringList.add("立即开始");
+    }
+
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent event) {
+        String pkgName = event.getPackageName().toString();
+        Log.i("yyz", "onAccessibilityEvent: " + pkgName);
+        if ("com.android.systemui".equals(pkgName) || "com.miui.securitycenter".equals(pkgName)) {
+            performClick(findViewByEqualsText());
+        }
+    }
+
+    @Override
+    public void onInterrupt() {
+
+    }
+
+    /**
+     * 根据getRootInActiveWindow查找和当前text相等的控件
+     */
+    @Nullable
+    public List<AccessibilityNodeInfo> findViewByEqualsText() {
+        List<AccessibilityNodeInfo> listOld = findViewByContainsText();
+        if (listOld == null || listOld.isEmpty()) {
+            return null;
+        }
+        ArrayList<AccessibilityNodeInfo> listNew = new ArrayList<>();
+        for (AccessibilityNodeInfo ani : listOld) {
+            if (ani.getText() != null) {
+                String s = ani.getText().toString();
+                if (stringList.get(0).equals(s) || stringList.get(1).equals(s)) {
+                    listNew.add(ani);
+                } else {
+                    ani.recycle();
+                }
+            }
+        }
+        return listNew;
+    }
+
+    /**
+     * 根据getRootInActiveWindow查找包含当前text的控件
+     */
+    @Nullable
+    public List<AccessibilityNodeInfo> findViewByContainsText() {
+        AccessibilityNodeInfo info = getRootInActiveWindow();
+        if (info == null) return null;
+        List<AccessibilityNodeInfo> list = new ArrayList<>();
+        for (String s : stringList) {
+            list.addAll(info.findAccessibilityNodeInfosByText(s));
+        }
+        info.recycle();
+        return list;
+    }
+
+    private boolean performClick(List<AccessibilityNodeInfo> nodeInfos) {
+        if (nodeInfos != null && !nodeInfos.isEmpty()) {
+            AccessibilityNodeInfo node;
+            for (int i = 0; i < nodeInfos.size(); i++) {
+                node = nodeInfos.get(i);
+                // 获得点击View的类型
+                // 进行模拟点击
+                if (node.isEnabled()) {
+                    Log.i("yyz", "performClick: 点");
+                    return node.performAction(AccessibilityNodeInfo.ACTION_CLICK);
+                }
+            }
+        }
+        return false;
+    }
+}
